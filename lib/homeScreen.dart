bdbad84d4f720e3057e333789510d82f5f335fe4
Raman Daksh <ramandaksh6161@gmail.com>
2024-03-06 14:24:58 +0530

    updated

--- lib/homeScreen.dart
+++ lib/homeScreen.dart
@@ -30,150 +30,195 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
         body: Center(
           child: Container(
-            height: MediaQuery.of(context).size.height * .7,
+            height: MediaQuery.of(context).size.height * .8,
             width: MediaQuery.of(context).size.width * .8,
             //margin: const EdgeInsets.only(top: 100, bottom: 100, left: 50, right: 50),
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(20),
               color: Colors.deepPurple,
             ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    color: Colors.deepPurple,
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(10),
-                    child: Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(
-                            MediaQuery.of(context).size.height * .2),
-                        border: Border.all(
-                          color: Colors
-                              .deepPurpleAccent, // Choose your desired border color
-                          width: 3.0, // Choose your desired border width
-                        ),
-                      ),
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.deepPurple,
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(10),
                       child: Container(
-                        width: MediaQuery.of(context).size.height * .3,
-                        height: MediaQuery.of(context).size.height * .3,
-                        decoration: const BoxDecoration(
-                          shape: BoxShape.circle,
-                          image: DecorationImage(
-                            alignment: Alignment.center,
-                            image: AssetImage('images/image.jpg'),
-                            fit: BoxFit.cover,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(
+                              MediaQuery.of(context).size.height * .2),
+                          border: Border.all(
+                            color: Colors
+                                .deepPurpleAccent, // Choose your desired border color
+                            width: 3.0, // Choose your desired border width
+                          ),
+                        ),
+                        child: Container(
+                          width: MediaQuery.of(context).size.height * .3,
+                          height: MediaQuery.of(context).size.height * .3,
+                          decoration: const BoxDecoration(
+                            shape: BoxShape.circle,
+                            image: DecorationImage(
+                              alignment: Alignment.center,
+                              image: AssetImage('images/image.jpg'),
+                              fit: BoxFit.cover,
+                            ),
                           ),
                         ),
                       ),
                     ),
                   ),
-                ),
-                SizedBox(height: MediaQuery.of(context).size.height * .03),
-                Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          'Name: ',
-                          style: Theme.of(context).textTheme.titleMedium,
-                        ),
-                        Text(
-                          "Raman Daksh",
-                          style: Theme.of(context).textTheme.titleMedium,
-                        ),
-                      ],
-                    ),
-                    SizedBox(
-                      height: MediaQuery.of(context).size.height * 0.02,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          'About: ',
-                          style: Theme.of(context).textTheme.titleMedium,
-                        ),
-                        Text(
-                          "Cool and Calm",
-                          style: Theme.of(context).textTheme.titleMedium,
-                        ),
-                      ],
-                    ),
-                    SizedBox(
-                      height: MediaQuery.of(context).size.height * 0.02,
-                    ),
-                    Container(
-                      color: Colors.white,
-                      padding: const EdgeInsets.only(top: 10, bottom: 10),
-                      margin: const EdgeInsets.only(top: 10, bottom: 10),
-                      child: Row(
+                  SizedBox(height: MediaQuery.of(context).size.height * .03),
+                  Column(
+                    children: [
+                      const Row(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          GestureDetector(
-                            onTap: () {
-                              launchUrl(fb);
-                            },
-                            child: Image.asset(
-                              'images/facebook.png',
-                              width: 30,
-                              height: 30,
-                            ),
-                          ),
-                          const SizedBox(width: 10),
-                          GestureDetector(
-                            onTap: () {
-                              launchUrl(tw);
-                            },
-                            child: Image.asset(
-                              'images/twitter.png',
-                              width: 30,
-                              height: 30,
-                            ),
-                          ),
-                          const SizedBox(width: 10),
-                          GestureDetector(
-                            onTap: () {
-                              launchUrl(ig);
-                            },
-                            child: Image.asset(
-                              'images/instagram.png',
-                              width: 30,
-                              height: 30,
-                            ),
-                          ),
-                          const SizedBox(width: 10),
-                          GestureDetector(
-                            onTap: () {
-                              launchUrl(git);
-                            },
-                            child: Image.asset(
-                              'images/github.png',
-                              width: 30,
-                              height: 30,
-                            ),
-                          ),
-                          const SizedBox(width: 10),
-                          GestureDetector(
-                            onTap: () {
-                              launchUrl(li);
-                            },
-                            child: Image.asset(
-                              'images/linkedin.png',
-                              width: 30,
-                              height: 30,
+                          Text(
+                            "Raman Daksh",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 35,
                             ),
                           ),
                         ],
                       ),
-                    )
-                  ],
-                ),
-              ],
+                      const Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "Developer",
+                            style: TextStyle(
+                              color: Color.fromARGB(255, 221, 209, 255),
+                              fontWeight: FontWeight.bold,
+                              fontSize: 30,
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.02,
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        padding: const EdgeInsets.only(
+                            top: 10, bottom: 10, left: 10, right: 10),
+                        child: Wrap(
+                          alignment: WrapAlignment.center,
+                          spacing: 10,
+                          children: [
+                            GestureDetector(
+                              onTap: () {
+                                launchUrl(fb);
+                              },
+                              child: Image.asset(
+                                'images/facebook.png',
+                                width: 30,
+                                height: 30,
+                              ),
+                            ),
+                            const SizedBox(width: 10),
+                            GestureDetector(
+                              onTap: () {
+                                launchUrl(tw);
+                              },
+                              child: Image.asset(
+                                'images/twitter.png',
+                                width: 30,
+                                height: 30,
+                              ),
+                            ),
+                            const SizedBox(width: 10),
+                            GestureDetector(
+                              onTap: () {
+                                launchUrl(ig);
+                              },
+                              child: Image.asset(
+                                'images/instagram.png',
+                                width: 30,
+                                height: 30,
+                              ),
+                            ),
+                            const SizedBox(width: 10),
+                            GestureDetector(
+                              onTap: () {
+                                launchUrl(git);
+                              },
+                              child: Image.asset(
+                                'images/github.png',
+                                width: 30,
+                                height: 30,
+                              ),
+                            ),
+                            const SizedBox(width: 10),
+                            GestureDetector(
+                              onTap: () {
+                                launchUrl(li);
+                              },
+                              child: Image.asset(
+                                'images/linkedin.png',
+                                width: 30,
+                                height: 30,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.02,
+                      ),
+                      const Text(
+                        "Technologies",
+                        style: TextStyle(
+                          color: Color.fromARGB(255, 221, 209, 255),
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                        ),
+                      ),
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.02,
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        padding: const EdgeInsets.only(
+                            top: 10, bottom: 10, left: 10, right: 10),
+                        child: Wrap(
+                          alignment: WrapAlignment.center,
+                          spacing: 10,
+                          children: [
+                            Image.asset('images/flutter.png',
+                                width: 30, height: 30),
+                            Image.asset('images/dart.png',
+                                width: 30, height: 30),
+                            Image.asset('images/firebase.png',
+                                width: 30, height: 30),
+                            Image.asset('images/python.png',
+                                width: 30, height: 30),
+                            Image.asset('images/reactjs.png',
+                                width: 30, height: 30),
+                            Image.asset('images/wordpress.png',
+                                width: 30, height: 30),
+                            Image.asset('images/shopify.png',
+                                width: 30, height: 30),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         )
